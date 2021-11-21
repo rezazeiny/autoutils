95c79b7e7aa9ae4af4dea95ae58c6a811bf88382
Reza Zeiny <rezazeiny1998@gmail.com>
2021-11-21 14:27:38 +0330

    start project

--- setup.py
+++ setup.py
@@ -36,10 +36,11 @@ setup(
     long_description_content_type="text/markdown",
     url="https://github.com/rezazeiny/autoutils",
     packages=packages,
+    platforms=['linux'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
-        "Operating System :: Linux",
+        "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
 )
